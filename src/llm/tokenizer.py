93158cc21d274e1330c7ab94bea8a4a40545de52
ki8yk8 <ki8yk8@gmail.com>
2025-12-31 10:23:50 +0545

    adds gpt tokenizer full support

--- src/llm/tokenizer.py
+++ src/llm/tokenizer.py
@@ -1,28 +1,21 @@
 from transformers import AutoTokenizer
 
 model_name = "openai-community/gpt2"
+class GPTTokenizer:
+	tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-# initializing tokenizer for GPT-2
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+	@classmethod
+	def get_tokens(self, sentence):
+		return self.tokenizer.tokenize(sentence)
 
-# simple tokenizer usage
-tokenizer_op = tokenizer("The capital city of France is")
-print(tokenizer_op["input_ids"])
-print(tokenizer_op["attention_mask"])
-
-# get an array of tokens
-tokens = tokenizer.tokenize("The capital city of France is")
-
-# get an array of token ids
-ids = tokenizer.convert_tokens_to_ids(tokens)
-print(ids)
-
-# decoding token ids
-decoded_string = tokenizer.decode(ids)
-print(decoded_string)
-
-# batching sequences; for returning pytroch tensor instead of tensor use return_tensors="pt"
-batch_tokeizer_op = tokenizer([
-	"This is the first word",
-	"This is the second word",
-], return_tensors="pt")
+	@classmethod
+	def token2id(self, tokens):
+		return self.tokenizer.convert_tokens_to_ids(tokens)
+	
+	@classmethod
+	def id2token(self, tokens):
+		return self.tokenizer.decode(tokens)
+	
+	@classmethod
+	def batch_tokenization(self, sentences):
+		return self.tokenizer(sentences)
