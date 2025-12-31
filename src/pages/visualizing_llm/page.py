1746bfaad989b46e5f98323dcf2c7fa22d03342d
ki8yk8 <ki8yk8@gmail.com>
2025-12-31 12:09:48 +0545

    modualrizes the interactive sessions

--- src/pages/visualizing_llm/page.py
+++ src/pages/visualizing_llm/page.py
@@ -1,6 +1,6 @@
-from dash import html, dcc, callback, Output, Input, State
+from dash import html
 from ...markdown import markdown_to_dash
-from ...llm import GPTTokenizer
+from .interactive.tokens import tokenization_interactive
 
 # title of the interactive blog
 title = html.H1(children="Visualizing LLMs Step by Step")
@@ -24,58 +24,6 @@ To solve this issue, we use tokenization. Tokenization is the process to convert
 
 You can use the interactive menu below, type your sentence and see how tokens are formed.
 """)
-
-tokenization_interactive = html.Section(
-	children=[
-		dcc.Input(value="Tokenizing is fun.", type="text", placeholder="Type words here and see it tokenize", id="token-input", n_submit=0),
-		html.Small(children="Press enter to see the tokens", className="hint"),
-		html.Div(id="token-output", className="code"),
-		html.Div(children=[
-			html.P(children=[
-				html.Span(children="Tokens: ", className="u-bold"),
-				html.Span(children="0", id="token-count")
-			]),
-			html.P(children=[
-				html.Span(children="Words: ", className="u-bold"),
-				html.Span(children="0", id="word-count")
-			])
-		], className="u-w-100 u-flex u-justify-between")
-	],
-	className="interactive"
-)
-
-@callback(
-	Output("token-output", "children"),
-	Output("token-count", "children"),
-	Output("word-count", "children"),
-	Input("token-input", "n_submit"),    # trigger
-	State("token-input", "value"),    # state
-	prevent_initial_call=False,
-)
-def tokenize_on_enter(n_submit, text):
-	if not text:
-		return "", "0", "0"
-
-	# simple word wise tokenization
-	tokens = GPTTokenizer.get_tokens(text)
-
-	html_tokens_list = []
-	for token in tokens:
-		if token.startswith("Ġ"):
-			html_tokens_list.append(
-				html.Span(children=token[1:], className="token token--space")
-			)
-		else:
-			html_tokens_list.append(
-				html.Span(children=token, className="token")
-			)
-
-	return (
-		html.Div(children=html_tokens_list, className="tokens_display"), 
-		str(len(html_tokens_list)), 
-		str(len(text.split()))
-	)
-
 
 page = html.Main(
 	children=[
